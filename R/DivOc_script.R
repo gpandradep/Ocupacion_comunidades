c0492f68c2415effdc7eff2a016aa0476816c2bb
gpandradep <gpandradep@unal.edu.co>
2022-04-30 19:10:19 -0500

    Script Div occ
    Actualizando el script de div.occ

--- R/DivOc_script.R
+++ R/DivOc_script.R
@@ -20,31 +20,78 @@ library(beepr) # Opcional para avisar R termine
 library(tictoc) # Opcional para tomar el tiempo de la función
 
 # 2. Cargar los datos ----
-files <- list.files("data/", full.names = T, pattern="*.csv") # lista de arhcivos.csv guardados en la carpeta "data
-hists <- lapply(files, read.csv) # Leer cada archivo como un csv
 
-# Juntar todas las bases de datos
-data <- hists %>% 
-  reduce(full_join, by="X") %>% # Aquí los unimos por el nombre de la estación de muestreo
-  select(-X) # Elimino la columna de estación
-view(data)
 
-# Ahora cargamos las covariables
-covars <- read.csv("Data/Covs/stdcovs_OC.csv")
+# Cargamos la tabla de registros de las especies
+registers <-  read.csv("Data/Survey/recordTable_OC.csv")
+table(registers$Species)
+
+
+
+# Cargamos la tabla de operación de cámaras
+CToperation <-  read.csv("Data/Survey/CTtable_OC.csv") 
+
+# Generamos la matríz de operación de las cámaras
+
+camop <- camtrapR::cameraOperation(CTtable= CToperation, # Tabla de operación
+                                   stationCol= "Station", # Columna que define la estación
+                                   setupCol= "Setup_date", #Columna fecha de colocación
+                                   retrievalCol= "Retrieval_date", #Columna fecha de retiro
+                                   hasProblems= T, # Hubo fallos de cámaras
+                                   dateFormat= "%Y-%m-%d") # Formato de las fechas
+
+
+
+# Función para generar las historias de detección para todas las especies seleccionadas
+
+DetHist_list <- lapply(unique(registers$Species), FUN = function(x) {
+  detectionHistory(
+    recordTable         = registers, # Tabla de registros
+    camOp                = camop, # Matriz de operación de cámaras
+    stationCol           = "Station",
+    speciesCol           = "Species",
+    recordDateTimeCol    = "DateTimeOriginal",
+    recordDateTimeFormat  = "%d/%m/%Y",
+    species              = x,     # la función reemplaza x por cada una de las especies
+    occasionLength       = 10,  # Colapso de las historias a 10 días
+    day1                 = "station", #inicie en la fecha de cada estación
+    datesAsOccasionNames = FALSE,
+    includeEffort        = TRUE,
+    scaleEffort          = TRUE,
+    timeZone             = "America/Mexico_City" 
+  )}
+)
+
+# Se genera una lista con cada historia de detección y el esfuerzo de muestreo, ahora le colocaremos los nombres para saber a cual especie corresponde
+names(DetHist_list) <- unique(registers$Species)
+
+# Finalmente creamos una lista nueva donde estén solo las historias de detección
+ylist <- lapply(DetHist_list, FUN = function(x) x$detection_history)
+
+# Todas las historias deben tener el mismo número de sitios y de ocasiones de muestreo
+
+data <- ylist %>% # Lista con los datos
+  reduce(cbind) # Unimos las historias de captura en un solo dataframe
+
+# Ahora cargamos las covariables (previamente estandarizadas)
+covars <- read.csv("Data/Covs/stdcovs_OC.csv") %>% 
+  select(-"...1", -X, -Station, -Cam, -Cluster) #
+
 
 # 3. Modelos de abundancia ----
 # 3.1 Modelos básicos ----
 cam_diver <- diversityoccu(pres = data, # La matriz de datos
-                           sitecov = covs, # Covariables de sitio
+                           sitecov = covars, # Covariables de sitio
                            obscov = NULL, # no tenemos covariables de observación,
-                           spp = 16, # Número de especies
-                           form = ~ Effort + Slope ~ SATVI, # Formula del modelo p(), lambda()
+                           spp = 17, # Número de especies
+                           form = ~ Effort + Slope ~ Dcrops, # Formula del modelo p(), lambda()
                            dredge = FALSE # En este primer ejemplo no usaremos AIC
 )
 
-# Veamos el modelo de la especie 2
 
-cam_diver$models[[2]] # Modelo para la especie 2
+# Veamos el modelo de la especie 9
+
+cam_diver$models[[9]] # Modelo para la especie 9
 
 # El problema es que no sabemos si todas las especies responden de la misma manera a las covariables que usamos. Debemos hacer una inferencia multi-modelo para escoger el mejor modelo para cada especie
 
@@ -52,26 +99,28 @@ cam_diver$models[[2]] # Modelo para la especie 2
 # Vamos a correr de nuevo la función pero esta vez escogerá el mejor modelo para cada especie
 tic()
 cam_diver_AIC <- diversityoccu(pres = data, # La matriz de datos
-                           sitecov = covs, # Covariables de sitio
+                           sitecov = covars, # Covariables de sitio
                            obscov = NULL, # no tenemos covariables de observación,
-                           spp = 16, # Número de especies
-                           form = ~ Effort + Slope ~ SATVI, # Formula del modelo p(), lambda()
+                           spp = 17, # Número de especies
+                           form = ~ Effort + Slope ~ Dcrops, # Formula del modelo p(), lambda()
                            dredge = TRUE # escoge los mejores modelos con AIC
 ); beep(sound = 8)
 toc() 
+
+
 
 # El código puede tardar mucho más dependiendo de la cantidad de datos, por lo que es mejor guardar el objeto en R para no correrlo cada vez que abramos el script.
 # save(cam_diver_AIC, file = "results/diver_AIC.R") # guarda el objeto en la carpeta results
 
 # load("results/diver_AIC.R") # Carga los resultados
 
-cam_diver_AIC$models[[2]] # El nuevo modelo para la sp2
+cam_diver_AIC$models[[9]] # El nuevo modelo para la sp9
 
 # 3.3 Gráfico de predicción ----
-# Gráfico de predicción para la especie 11
+# Gráfico de predicción para la especie 3
 responseplot.abund( batch = cam_diver_AIC, # objeto creado con diversityoccu
-                    spp = 11, # número o nombre de la sp
-                    variable= SATVI # variable  
+                    spp = 3, # número o nombre de la sp
+                    variable= Dcrops # variable  
 )
 
 # 4. Modelando la diversodad ---- 
@@ -88,7 +137,7 @@ toc()
 AICtab <- glm.div$Table
 
 # 4.1 Respuesta Gráfica de la diversidad a distintas  variables ----
-responseplot.diver(glm.div, SATVI)
+responseplot.diver(glm.div, Dcrops)
 responseplot.diver(glm.div, Dpop_G)
 responseplot.diver(glm.div, Slope)
 
@@ -96,37 +145,37 @@ responseplot.diver(glm.div, Slope)
 
 # Shanon
 cam_diver_sh <- diversityoccu(pres = data, 
-                              sitecov = covs, 
+                              sitecov = covars, 
                               obscov = NULL, 
-                              spp = 16, 
-                              form = ~ Effort + Slope ~ SATVI,
+                              spp = 17, 
+                              form = ~ Effort + Slope ~ Dcrops,
                               dredge = TRUE, 
                               index = "shannon" #<<
 )
-#save(cam_diver_sh, file = "results/shanon.R")
+save(cam_diver_sh, file = "results/shanon.R")
 
 
 # simpson
 cam_diver_sim <- diversityoccu(pres = data, 
-                               sitecov = covs, 
+                               sitecov = covars, 
                                obscov = NULL, 
-                               spp = 16, 
-                               form = ~ Effort + Slope ~ SATVI,                            dredge = TRUE,  
+                               spp = 17, 
+                               form = ~ Effort + Slope ~ Dcrops,                            dredge = TRUE,  
                                index = "simpson" #<<
 )
-#save(cam_diver_sim, file = "results/simpson.R")
+save(cam_diver_sim, file = "results/simpson.R")
 
 
 # inverso de simpson
 cam_diver_inv <- diversityoccu(pres = data, 
-                               sitecov = covs, 
+                               sitecov = covars, 
                                obscov = NULL, 
-                               spp = 16, 
-                               form = ~ Effort + Slope ~ SATVI,                            dredge = TRUE,  
+                               spp = 17, 
+                               form = ~ Effort + Slope ~ Dcrops,                            dredge = TRUE,  
                                index = "invsimpson" #<<
 ); beep(sound = 8)
 
-#save(cam_diver_inv, file = "results/invsimpson.R")
+save(cam_diver_inv, file = "results/invsimpson.R")
 
 # 5. Calculamos número efectivo de especies ----
 
@@ -143,13 +192,15 @@ q2 <- hill_taxa(hill_data, q=2)
 hill_div <- data.frame(q0=q0, q1=q1, q2=q2)
 
 # Juntamos con las covariables
-glm_hill <- cbind(hill_div, covs)
+glm_hill <- cbind(hill_div, covars)
 
 # Ajustamos un GLM
-glm_q1 <- glm(q1~ Dpop_G, family = gaussian, data = glm_hill)
+glm_q1 <- glm(q1~ Dcrops, family = gaussian, data = glm_hill)
 
 # Gráfica del modelo
-plot_q1 <- ggpredict(glm_q1, terms = "Dpop_G")
+plot_q1 <- ggpredict(glm_q1, terms = "Dcrops")
 plot(plot_q1)+
   labs(y= "Diversidad q1", x= "Distancia a poblados (estandarizado)")
   theme_classic()
+  
+plot_q1
